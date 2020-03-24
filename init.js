27e0405cb5f5141e125196dab7fcdacc99bb5546
Jonathan Silva Viana <41023159+JonathanSilvaViana@users.noreply.github.com>
2020-03-24 19:12:58 -0300

    changes applied
    functional filter search

--- init.js
+++ init.js
@@ -25,3 +25,18 @@ $('#download-button').click(function(){
     M.toast({html: 'Got ya!', classes: 'rounded white-text green lighten-3 flow-text software-template-name'});
 });
 
+//converte table para ul li
+/*
+$(document).ready(function() {
+    var ul = $("<ul class='highlight centered responsive-table striped green accent-2 list'>");
+    $("table tr").each(function(){
+        var li = $("<li>")
+        $("th, td", this).each(function(){
+            var p = $("<p class='white-text'>").html(this.innerHTML);
+            li.append(p);
+        });
+        ul.append(li);
+    })    
+    $("table").replaceWith(ul);    
+});
+*/
